fbd7ec51a63908971cd333426d6b5a145ce9c679
James Jamail <james@jamesjamail.com>
2019-10-25 11:22:26 -0500

    fixed ecosystem.config file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,9 +6,9 @@ module.exports = {
     deploy: {
       production: {
         user: 'ubuntu',
-        host: 'ec2-13-59-243-41.us-east-2.compute.amazonaws.com',
+        host: 'ec2-13-59-243-41.us-east-2.compute.amazonaws.com/',
         key: '~/.ssh/tutorial.pem',
-        ref: 'origin/separateComponents',
+        ref: 'origin/dev',
         repo: 'git@github.com:Amishon/Search_Cart.git',
         path: '/home/ubuntu/Search_Cart',
         'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'

c2c0a68af08b5b7e87d6d1089e933442dbff961d
James Jamail <james@jamesjamail.com>
2019-10-24 19:44:48 -0500

    changed AWS DNS to current

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
     deploy: {
       production: {
         user: 'ubuntu',
-        host: 'ec2-18-218-63-51.us-east-2.compute.amazonaws.com',
+        host: 'ec2-3-15-176-91.us-east-2.compute.amazonaws.com',
         key: '~/.ssh/tutorial.pem',
         ref: 'origin/dev',
         repo: 'git@github.com:Amishon/Search_Cart.git',

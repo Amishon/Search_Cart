e31cea3b4e0c7298c56b81f2e7bc99109f3f1168
James Jamail <james@jamesjamail.com>
2019-10-25 11:28:24 -0500

    webpack compiled now

--- src/client/index.js
+++ src/client/index.js
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom';
 import SearchBar from './components/searchBar.jsx';
 import Cart from './components/Cart.jsx';
 
-<<<<<<< HEAD
+
 ReactDOM.render(<App />, document.getElementById('jj-searchBar'));
 ReactDOM.render(<App />, document.getElementById('jj-cart'));
-=======
-ReactDOM.render(<SearchBar />, document.getElementById('jj-searchBar'));
-ReactDOM.render(<Cart />, document.getElementById('jj-addToCart'));
->>>>>>> df4ab7f59a2f3eab5e9b9fa2137651de047c56de

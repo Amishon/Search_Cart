1c85ef77036c8de8bd0d3fccdfdc643e1deea9df
James Jamail <james@jamesjamail.com>
2019-10-26 11:52:42 -0500

    debugging url routes

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, '../dist/')));
+app.use(express.static(path.join(__dirname, '/../dist/')));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
